859029ce01f2e0a01df58c3cbc47ebea6075759c
BesthianGR <92615033+BesthianGR@users.noreply.github.com>
2025-10-01 22:48:11 +0700

    Add files via upload

---
+++ Modul 1/Code C++/Guided2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Masukkan sebuah karakter: ";
+    cin >> ch; 
+    cout << "Karakter yang Anda masukkan adalah: " << ch << endl;
+    return 0;
+}
